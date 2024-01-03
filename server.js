c6cf4732f7f80678d24e8f1d518477a41ca9bac7
Lledxx <Joaquinlledxx@outlook.com.ar>
2024-01-03 02:02:49 -0300

    archivos que faltaron subir

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 import express from "express";
 
 const server = express();
-
 const PORT = 8080;
-const ready = () => console.log("server ready on port" + PORT);
+
+const ready = () => console.log("Server ready on port " + PORT);
+
+const path = "/";
+
+const functionRead = (req, res) => {
+  res.status(200).send("My first Express server");
+};
+
+server.get(path, functionRead);
 
 server.listen(PORT, ready);
